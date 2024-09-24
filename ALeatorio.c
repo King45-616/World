a05d95e78f52366504eae0dabf9ad6367bcb5540
King <timeline562@gmail.com>
2024-09-24 10:54:05 -0300

    Add files via upload

---
+++ ALeatorio.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+       int a = 6, b = 20;
+
+       int soma = a + b;
+       printf("soma %d e %d é %d\n", a, b, soma);
+
+       return 0;
+
+}
---
+++ roberto.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int numero1 = 6; //essa variável a
+    int numero2 = 20;
+    int soma;
+    char letra = '1';
+char nome[7] = {"roberto"};
+printf("%c", letra);
+soma = numero1 + numero2;
+printf( "%d", soma);
+}
